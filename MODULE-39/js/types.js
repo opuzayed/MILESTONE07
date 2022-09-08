09db9acba34edd79ae54d79747701f61b945027a
opuzayed <zayedopu4933@gmail.com>
2022-09-08 11:14:47 +0600

    8 ways to get undefined

--- MODULE-39/js/types.js
+++ MODULE-39/js/types.js
@@ -2,4 +2,26 @@ let a = 12;
 let b = 'opu';
 let age = [12, 14, 17];
 let student = {age : 12, name : 'mofiz'};
-console.log(typeof a, typeof b, Array.isArray(age), typeof student);
+//console.log(typeof a, typeof b, Array.isArray(age), typeof student);
+
+//----------re-assign-value----------
+let x = 10;
+let y = x;
+//console.log(x, y);
+y = 12;
+//console.log(x, y);
+
+/* -------------8 ways to get undefined-------------
+    
+    1.variable declared but not assign will give undefined
+    2.function declared but no return will give undefined
+    3.function parameter not pass will give undefined
+    4.if condition return nothing on the right side will give undefined
+    5.property that doesn't exist on an object will give undefined.
+    6.accessing array element outide of the index rnge will give undefined
+    7.delete an element inside an array will give undefined.
+    8.where is no value but we set a value then we should use null instead of undefined
+ 
+*/
+
+
